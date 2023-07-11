9c15ba6d88464926c5df54c8f6b1e77655fba00f
adiletexe <108264958+adiletexe@users.noreply.github.com>
2023-07-12 01:17:45 +0600

    Update settings.py

--- newagrimo/settings.py
+++ newagrimo/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'static/main/images'
