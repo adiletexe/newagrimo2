b6a426ec602ffed9b28504d7f91d4d4ad1f87101
adiletexe <108264958+adiletexe@users.noreply.github.com>
2023-08-17 11:46:39 +0600

    Update settings.py

--- newagrimo/settings.py
+++ newagrimo/settings.py
@@ -140,3 +140,5 @@ MEDIA_ROOT = BASE_DIR / 'static/main/images'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_ALLOW_ALL_ORIGINS = True
