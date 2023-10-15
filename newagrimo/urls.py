6dc491f46679486855c83f256dc598ea094413ee
adiletexe <108264958+adiletexe@users.noreply.github.com>
2023-10-15 22:11:48 +0600

    Update urls.py

--- newagrimo/urls.py
+++ newagrimo/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
     path('', views.index, name='index'),
     path('signup', views.signupsystem, name='signupsystem'),
     path('login', views.loginsystem, name='loginsystem'),
