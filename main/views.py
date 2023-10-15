2ff0d9a44eb8f2720f938469744cae085c299816
adiletexe <108264958+adiletexe@users.noreply.github.com>
2023-10-15 22:09:44 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -36,7 +36,7 @@ from langchain.memory import ConversationBufferMemory
 os.environ['OPENAI_API_KEY'] = "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"
 
 # location of the pdf file/files.
-doc_reader = PdfReader('content/wellbeing-team-cbt-workshop-booklet-2016.pdf')
+doc_reader = PdfReader('content/agrimostrain.pdf')
 
 # read data from the file and put them into a variable called raw_text
 raw_text = ''
