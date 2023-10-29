c6f3dddcdf581a4815ed4e61c296acb78d9807bf
adiletexe <108264958+adiletexe@users.noreply.github.com>
2023-10-29 22:38:13 +0600

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Shapes, UserProfile, Sunradiation, Odor, Humidity, Raindrop, Temperature, Light, Moisture, Pressure, Shop, Specialists, Review
-
+from .models import Shapes, UserProfile, Sunradiation, Odor, Humidity, Raindrop, Temperature, Light, Moisture, Pressure, Shop, Specialists, Review, Info, Table, Statistics
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Odor)
@@ -15,8 +14,9 @@ admin.site.register(Specialists)
 admin.site.register(Review)
 admin.site.register(Sunradiation)
 admin.site.register(Shapes)
-
-
+admin.site.register(Info)
+admin.site.register(Table)
+admin.site.register(Statistics)
 
 
 
