8413c63520e1e2c2d60ee7d5083d70900afdbf05
adiletexe <108264958+adiletexe@users.noreply.github.com>
2023-10-29 09:15:13 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -23,6 +23,7 @@ from random import randint
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 import os
 
 @api_view(['POST'])
